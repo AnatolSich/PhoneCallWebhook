ec8579d39db49dae8d553e5a0b6437d01178cc1c
anatoly sichkar <Sichkar.Anatoly@innovecs.com>
2020-02-19 13:27:55 +0200

    New port configured, twilio lib added

--- src/main/java/com/example/PhoneCallWebhook/controller/WebhookController.java
+++ src/main/java/com/example/PhoneCallWebhook/controller/WebhookController.java
@@ -1,15 +1,28 @@
 package com.example.PhoneCallWebhook.controller;
 
+import com.twilio.twiml.VoiceResponse;
+import com.twilio.twiml.voice.Play;
+import com.twilio.twiml.voice.Say;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Controller
 public class WebhookController {
 
-    @GetMapping("/call")
+    private AtomicInteger callerNumber = new AtomicInteger();
+
+    @GetMapping(path="/call", produces="application/xml")
     @ResponseBody
     public String respondToPhoneCall() {
-        return "Welcome to Spring Boot";
+
+        VoiceResponse.Builder voiceBuilder = new VoiceResponse.Builder();
+
+        Say greeting = new Say.Builder("Hello caller number " + callerNumber.incrementAndGet()).build();
+        Play music = new Play.Builder("https://static.gilliard.lol/Might_As_Well_Be_Spring.mp3").build();
+
+        return voiceBuilder.say(greeting).play(music).build().toXml();
     }
 }
